1a3ea68d3e6933f34f91b4be97d12d175fcbe61f
DeveloperAlan <alanng95@hotmail.com>
2016-08-12 12:08:12 +1000

    push tasks and body for delete and check

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,25 +2,26 @@ import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
 
 import './body.html';
+import "./task.js";
 
 Template.body.helpers({
-	tasks() {
-		return Tasks.find({}, { sort: { createdAt: -1}});
-	}
+    tasks() {
+        return Tasks.find({}, { sort: { createdAt: -1 } });
+    }
 });
 
 Template.body.events({
-	'submit .new-task'(event) {
-		event.preventDefault();
+    'submit .new-task'(event) {
+        event.preventDefault();
 
-		const target = event.target;
-		const text = target.text.value;
+        const target = event.target;
+        const text = target.text.value;
 
-		Tasks.insert({
-			text, 
-			createdAt: new Date(),
-		});
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        });
 
-		target.text.value = '';
-	}
-})
+        target.text.value = '';
+    }
+});
